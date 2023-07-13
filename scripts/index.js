c70208ad2cff9328e93fabd626ca4657d8cc5e4c
Ilya Korolkov <yand3xe1@yandex.ru>
2023-07-13 12:22:00 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,27 @@
 const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.profile__open-popup');
+const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close');
 const popupToggle = function () {
   popup.classList.toggle('popup_opened');
+  nameInput.value = username.textContent;
+  jobInput.value = job.textContent;
 }
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
+let formElement = document.querySelector('.form-personal');
+let nameInput = formElement.querySelector('.form-personal__field_type_name');
+let jobInput = formElement.querySelector('.form-personal__field_type_job');
+ 
+let username = document.querySelector('.profile__title');
+let job = document.querySelector('.profile__description');
 
 //отправка формы
 
-let formElement = document.querySelector('.popup__form');
-
-let nameInput = formElement.querySelector('.popup__form-name');
-let jobInput = formElement.querySelector('.popup__form-job');
-
 function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-    let nameValue = nameInput.value;
-    let jobValue = jobInput.value;
- 
-    let username = document.querySelector('.profile__title');
-    let job = document.querySelector('.profile__description');
-
-    username.textContent = nameValue;
-    job.textContent = jobValue;
+    evt.preventDefault();
+    username.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    popupToggle()
 }
 
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', handleFormSubmit);
